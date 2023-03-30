d68e629981ef6e4bcd6bd83f9d1ff1ee5a0b3b7f
jacobv123 <94700520+jacobv1234@users.noreply.github.com>
2023-03-30 16:35:11 +0100

    Delete Prototypes/Version 3 directory

--- Prototypes/Version 3/static/home.js
+++
@@ -1,8 +0,0 @@
-function goNow() {
-    console.log('http://' + location.hostname + '/weather')
-    if (location.hostname == 'localhost') {
-        window.location.href = 'http://localhost:' + location.port + '/weather'
-    } else {
-        window.location.href = 'http://' + location.hostname + '/weather'
-    }
-}
